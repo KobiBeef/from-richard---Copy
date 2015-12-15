852e3ee91b2be474b9e333c73f259fefec54683a
KobiBeef <ezekielbacungan@gmail.com>
2015-12-15 21:18:01 +0800

    "richard calc"
    from java to python richard school test calculator

---
+++ cal2.py
@@ -0,0 +1,136 @@
+import random
+
+def scan_input():
+	pass
+
+class MathDrill():
+	def __init__(self):
+		self.no_item = ""
+		self.operator = ""
+	
+	def isCheck(self, num_1, num_2, ans, opt):
+		if opt==1: 
+			if (num_1 + num_2) == ans:
+				return True
+		elif opt==2: 
+			if((n1-n2)==ans):
+				return True
+		elif opt==3: 
+			if((n1*n2)==ans):
+				return True
+		elif opt==4: 
+			if((n1/n2)==ans):
+				return True
+		else:
+			return False
+
+	# which is a better design?
+		# - integerating the isCheck in the doFunctions? OR
+		# - separating the isCheck from the doFunctions?
+
+	def isPassed(self, score):
+		if score >= self.no_item/2:
+			print "You Passed!"
+		else:
+			print "You Failed!"
+
+	# reuse of the opt==1 variable from main3.py
+	def doAddtion(self, opt):
+		score = 0
+		# x=1
+		item_num = 1
+		# item_num starts with the value of 1
+		while item_num <= self.no_item:
+			num_1 = random.randint(1,10)
+			num_2 = random.randint(1,10)
+			ans = num_1 + num_2
+
+			print "%d. %d + %d = ?" % (item_num, num_1 ,num_2)
+			
+			#########################################
+			# filter user_ans_input to integers only#
+			# must not accept null/empty input      #
+			#########################################
+			while True:
+				user_ans_input = raw_input("ur answer is > ")
+				try:
+					if len(user_ans_input) == 0:
+						print "enter a value"
+					else:
+						user_ans_input_to_int = int(user_ans_input)
+						break
+				except:
+					print "enter integers only"
+
+			# if (user_ans_input == ans) == self.isCheck(num_1, num_2, ans ,1):
+			if self.isCheck(num_1, num_2, ans, opt=opt):
+			# this checks if the computer answer is correct
+				if ans == user_ans_input_to_int:
+				# this checks if user answer is correct
+					score+=1
+					print "That's Correct!"
+				else:
+					print "Sorry, That's Wrong!"
+			item_num+=1
+			# while exit if item_num > self.no_item
+
+		# use a isPassed function here
+		self.isPassed(score)
+
+		
+	def doSubtraction(self):
+		score=0
+		x=1
+		while x<=self.numbers:
+			n1=random.randint(1,10)
+			n2=random.randint(1,10)
+			ans=n1+n2
+			print "%d. %d - %d = ?" % (x,n1,n2)
+			if(self.isCheck(n1,n2,ans,2)):
+				score+=1
+				print "That's Correct!"
+			else:
+				print "Sorry, That's Wrong!"
+			if(score>=(self.numbers/2)):
+				print "You Passed!"
+			else:
+				print "You Failed!"
+			x+=1
+		
+	def doMultiplication(self):
+		score=0
+		x=1
+		while x<=self.numbers:
+			n1=random.randint(1,10)
+			n2=random.randint(1,10)
+			ans=n1+n2
+			print "%d. %d * %d = ?" % (x,n1,n2)
+			if(self.isCheck(n1,n2,ans,3)):
+				score+=1
+				print "That's Correct!"
+			else:
+				print "Sorry, That's Wrong!"
+			if(score>=(self.numbers/2)):
+				print "You Passed!"
+			else:
+				print "You Failed!"
+			x+=1
+		
+	def doDivision(self):
+		score=0
+		x=1
+		while x<=self.numbers:
+			n1=random.randint(1,10)
+			n2=random.randint(1,10)
+			ans=n1+n2
+			print "%d. %d / %d = ?" % (x,n1,n2)
+			if(self.isCheck(n1,n2,ans,4)):
+				score+=1
+				print "That's Correct!"
+			else:
+				print "Sorry, That's Wrong!"
+			if(score>=(self.numbers/2)):
+				print "You Passed!"
+			else:
+				print "You Failed!"
+			x+=1
---
+++ main3.py
@@ -0,0 +1,60 @@
+from cal2 import MathDrill
+rich=MathDrill()
+name=raw_input("Please Enter Your Name: ")
+print "\nHi, %s\n\nWelcome To The Math Drill and Practice Program" % name
+
+ch=0	
+while ch!=1:
+	while True:
+		opt = raw_input("\nPlease Select Operation:\nAddition:\t1\nSubtraction:\t2\nMultiplication:\t3\nDivision:\t4\n")
+		try:
+			if len(opt) == 0:
+				print "enter a value"
+			elif len(opt) > 1:
+				print "enter value 1, 2, 3, and 4 only"
+			else:
+				opt = int(opt)
+				if opt <= 0:
+					print "input positive numbers only"
+				elif opt > 4:
+					print "enter value 1, 2, 3, and 4 only"
+				else:
+					break
+		except:
+			print "enter integer only"		
+	# opt=int(raw_input("\nPlease Select Operation:\nAddition:\t1\nSubtraction:\t2\nMultiplication:\t3\nDivision:\t4\n"))
+	# put a input guardian for variable opt: should not accept string/s and interger below 1 and above 4
+	
+	while True:
+		total_item=raw_input("\nHow Many Items Would You Like To Answer? ")
+		total_item = total_item.split()
+		try:
+			if len(total_item) == 0:
+				print "enter how many items would you like to answer"
+			elif len(total_item) > 1:
+				print "enter only one value of items you would like to answer"
+			else:
+				total_item = int(total_item[0])
+				break
+		except :
+			print "enter integers only or any value above zero"
+	# total_item=int(raw_input("\nHow Many Items Would You Like To Answer? "))
+	# put a input guardian for the variable totalItem: should not accept string/s input
+
+	# would try to put the raw_input() filter to one function then call
+	# def filter_user_input
+
+	rich.no_item=total_item
+	if opt==1: 
+		rich.doAddtion(opt)
+	if opt==2: 
+		rich.doSubtraction()
+	if opt==3: 
+		rich.doMultiplication()
+	if opt==4: 
+		rich.doDivision()
+	ch=int(raw_input("\nWould You Like To Try Another? 1 for No 0 for Yes\n"))
+	# put an input guardian for variable ch: should not accept string/s and numbers below 0 and above 1
+	# ch variablae input exits or continous the while loop
+else:
+	print "\nThank You %s For Using The Program!\n\nSee You On The Next Math Drill...\n\n\nProgrammed by II-3\nGroup 6:\nMary Rose L. Moises\nMikarla Lyn M. Molina\nRichard A. Ongpin\nJane A. Paculan\nMary Gayle L. Padrinao" % name
